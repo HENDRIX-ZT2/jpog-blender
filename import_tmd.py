b8ade162018ee78c04e10929ff86cb520fe2b9a7
HENDRIX-ZT2 <arnfinn@spholderer.de>
2018-05-10 13:44:37 +0200

    Code Cleanup
    Remove semicolons

--- import_tmd.py
+++ import_tmd.py
@@ -41,32 +41,32 @@ def vec_roll_to_mat3(vec, roll):
 	#create a 3x3 matrix
 	bMatrix = mathutils.Matrix().to_3x3()
 
-	theta = 1.0 + nor[1];
+	theta = 1.0 + nor[1]
 
 	if (theta > THETA_THRESHOLD_NEGY_CLOSE) or ((nor[0] or nor[2]) and theta > THETA_THRESHOLD_NEGY):
 
-		bMatrix[1][0] = -nor[0];
-		bMatrix[0][1] = nor[0];
-		bMatrix[1][1] = nor[1];
-		bMatrix[2][1] = nor[2];
-		bMatrix[1][2] = -nor[2];
+		bMatrix[1][0] = -nor[0]
+		bMatrix[0][1] = nor[0]
+		bMatrix[1][1] = nor[1]
+		bMatrix[2][1] = nor[2]
+		bMatrix[1][2] = -nor[2]
 		if theta > THETA_THRESHOLD_NEGY_CLOSE:
 			#If nor is far enough from -Y, apply the general case.
-			bMatrix[0][0] = 1 - nor[0] * nor[0] / theta;
-			bMatrix[2][2] = 1 - nor[2] * nor[2] / theta;
-			bMatrix[0][2] = bMatrix[2][0] = -nor[0] * nor[2] / theta;
+			bMatrix[0][0] = 1 - nor[0] * nor[0] / theta
+			bMatrix[2][2] = 1 - nor[2] * nor[2] / theta
+			bMatrix[0][2] = bMatrix[2][0] = -nor[0] * nor[2] / theta
 		
 		else:
 			#If nor is too close to -Y, apply the special case.
-			theta = nor[0] * nor[0] + nor[2] * nor[2];
-			bMatrix[0][0] = (nor[0] + nor[2]) * (nor[0] - nor[2]) / -theta;
-			bMatrix[2][2] = -bMatrix[0][0];
-			bMatrix[0][2] = bMatrix[2][0] = 2.0 * nor[0] * nor[2] / theta;
+			theta = nor[0] * nor[0] + nor[2] * nor[2]
+			bMatrix[0][0] = (nor[0] + nor[2]) * (nor[0] - nor[2]) / -theta
+			bMatrix[2][2] = -bMatrix[0][0]
+			bMatrix[0][2] = bMatrix[2][0] = 2.0 * nor[0] * nor[2] / theta
 
 	else:
 		#If nor is -Y, simple symmetry by Z axis.
 		bMatrix = mathutils.Matrix().to_3x3()
-		bMatrix[0][0] = bMatrix[1][1] = -1.0;
+		bMatrix[0][0] = bMatrix[1][1] = -1.0
 
 	#Make Roll matrix
 	rMatrix = mathutils.Matrix.Rotation(roll, 3, nor)
